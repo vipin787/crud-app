e8e108ec6ef7933f0dfdbc410c401455b0393ca3
mdnizaam <ddinnizamu810@gmail.com>
2023-01-29 11:17:44 +0530

    react crud app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
     </>
   );
 }
